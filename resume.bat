fa90b2e7b963b2850a5bf4d62aead7e151d2181f
Νικόλας <naurgul@yahoo.gr>
2011-06-13 11:40:17 +0300

    Fixed resume launcher being too slow
    -Pointed resume.bat to the correct version of JRE, it runs as fast as within the IDE now
    -Removed class TestChess, if its functionality is needed it can be easily incorporated into PlayChess

--- resume.bat
+++ resume.bat
@@ -1,2 +1,2 @@
-java -classpath ".\bin;C:\Program Files (x86)\Java\LIBRARIES\ecj" ec.Evolve -checkpoint "playtest.2.gz"
+"C:\Program Files (x86)\Java\jre6\bin\java" -classpath ".\bin;C:\Program Files (x86)\Java\LIBRARIES\ecj" ec.Evolve -checkpoint "playtest.3.gz"
 pause
